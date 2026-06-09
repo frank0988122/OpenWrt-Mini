7898ac14f99918e8cf525b1432d4f09e8e91fec2
frank0988122 <frank0988@proton.me>
2026-06-09 16:54:45 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-# ShadowSocksR Plus+
 echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 
-# OpenClash
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >> feeds.conf.default
 
-# PassWall
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
 
-# Poweroff
 git clone --depth=1 https://github.com/WukongMaster/luci-app-poweroff.git package/luci-app-poweroff
 
-# Theme
 git clone --depth=1 https://github.com/WukongMaster/luci-theme-opentomcat.git package/luci-theme-opentomcat
